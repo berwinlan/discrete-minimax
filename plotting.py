e2fdb39b310277a9bcf9a5de33d4ddf8e41ec77a
Berwin Lan <blan@olin.edu>
2022-10-28 16:22:52 -0400

    Plot data in MATLAB
    Path is Berwin's Windows specific

--- plotting.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot as plt
-import pandas as pd
-
-minimax_data = pd.read_csv('data/minimax-data.csv')
-abpruning_data = pd.read_csv('data/abpruning-data.csv')
-
-minimax_data.plot()
